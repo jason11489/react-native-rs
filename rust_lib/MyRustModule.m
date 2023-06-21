a411c8a8774f8c2ca752a544829e5115dfd12989
jason11489 <donny11489@gmail.com>
2023-06-21 21:06:54 +0900

    add rust_lib && Object-C file

---
+++ rust_lib/MyRustModule.m
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+#import "React/RCTBridgeModule.h"
+#include "./rust_module.h"
+
+@interface MyRustModule : NSObject <RCTBridgeModule>
+
+@end
+
+@implementation MyRustModule
+
+// Write your Method to use
+
+@end
