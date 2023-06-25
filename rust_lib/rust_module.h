264873d5b93c4804043376a0954cbccf8bdcfb39
jason11489 <donny11489@gmail.com>
2023-06-25 23:09:04 +0900

    make new branch for zk-market circuit

---
+++ rust_lib/rust_module.h
@@ -0,0 +1,10 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int32_t add_numbers(int32_t a, int32_t b);
+
+void hello_world(void);
+
+void hello_devworld(void);
