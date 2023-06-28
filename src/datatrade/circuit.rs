dc785fdd60d635d99f914c22ab766beb0bf01a9f
jason11489 <donny11489@gmail.com>
2023-06-28 15:05:46 +0900

    add register data circuit

--- src/datatrade/circuit.rs
+++ src/datatrade/circuit.rs
@@ -4,21 +4,28 @@ use crate::gadget::{
         constraints::CRHSchemeGadget,
         mimc7::{self, constraints::MiMCGadget},
     },
-    merkle_tree::{self, constraints::ConfigGadget, Config, IdentityDigestConverter},
-    public_encryptions::{
-        elgamal::{self, constraints::ElGamalEncGadget, ElGamal},
-        AsymmetricEncryptionGadget,
-    },
     symmetric_encrytions::{
         constraints::SymmetricEncryptionGadget,
         symmetric::{
             self, constraints::SymmetricEncryptionSchemeGadget, SymmetricEncryptionScheme,
         },
+        SymmetricEncryption,
     },
 };
+use ark_crypto_primitives::snark::{CircuitSpecificSetupSNARK, SNARK};
+
+use ark_bn254::Bn254;
+use ark_groth16::Groth16;
+use ark_std::rand::RngCore;
+use ark_std::rand::SeedableRng;
+use ark_std::test_rng;
+use ark_std::UniformRand;
+use ark_std::Zero;
+
+use ark_crypto_primitives::Error;
 
 use ark_crypto_primitives::sponge::Absorb;
-use ark_ec::{AffineRepr, CurveGroup};
+use ark_ec::{bn::Bn, CurveGroup};
 use ark_ff::{Field, PrimeField};
 use ark_r1cs_std::prelude::*;
 use ark_r1cs_std::{fields::fp::FpVar, prelude::AllocVar};
@@ -44,7 +51,7 @@ where
     pub data: Option<C::BaseField>,
     pub k_data: Option<C::BaseField>,
     pub ct_r: Option<C::BaseField>,
-    pub ct_data: Option<Vec<C::BaseField>>,
+    pub ct_data: Option<C::BaseField>,
 
     pub _curve_var: PhantomData<GG>,
 }
@@ -61,6 +68,8 @@ where
         self,
         cs: ark_relations::r1cs::ConstraintSystemRef<C::BaseField>,
     ) -> ark_relations::r1cs::Result<()> {
+        //==============================================================================================================
+
         let rc = hashes::mimc7::Parameters {
             round_constants: self.rc,
         };
@@ -69,122 +78,198 @@ where
             &rc.clone(),
         )
         .unwrap();
+        //==============================================================================================================
+        //h_k_data == Hash(pk_peer_own || k_data)
 
-        let h_ct =
-            FpVar::new_input(ark_relations::ns!(cs, "h_ct"), || Ok(self.h_ct.unwrap())).unwrap();
         let h_k_data = FpVar::new_input(ark_relations::ns!(cs, "h_k_data"), || {
             Ok(self.h_k_data.unwrap())
         })
         .unwrap();
+
         let pk_peer_own = FpVar::new_input(ark_relations::ns!(cs, "pk_peer_own"), || {
             Ok(self.pk_peer_own.unwrap())
         })
         .unwrap();
 
+        let binding = self.k_data.unwrap();
+        let k_data_binding =
+            FpVar::new_witness(ark_relations::ns!(cs, "k_data"), || Ok(binding)).unwrap();
+
+        let hash_input = [pk_peer_own, k_data_binding].to_vec();
+        let result_h_k_data = MiMCGadget::<C::BaseField>::evaluate(&rc, &hash_input).unwrap();
+
+        result_h_k_data.enforce_equal(&h_k_data).unwrap();
+
         //==============================================================================================================
 
-        let data =
-            FpVar::new_witness(ark_relations::ns!(cs, "data"), || Ok(self.data.unwrap())).unwrap();
+        // h_ct == Hash(CT_data)
 
-        let k_data_1 = FpVar::new_witness(ark_relations::ns!(cs, "k_data_1"), || {
-            Ok(self.k_data.unwrap())
-        })
-        .unwrap();
+        let h_ct =
+            FpVar::new_input(ark_relations::ns!(cs, "h_ct"), || Ok(self.h_ct.unwrap())).unwrap();
 
-        let k_data_2 =
+        let binding = self.ct_data.clone().unwrap();
+        let ct_data_binding =
+            FpVar::new_witness(ark_relations::ns!(cs, "ct_data"), || Ok(binding)).unwrap();
+
+        let hash_input = [ct_data_binding].to_vec();
+        let result_h_ct = MiMCGadget::<C::BaseField>::evaluate(&rc, &hash_input).unwrap();
+
+        result_h_ct.enforce_equal(&h_ct).unwrap();
+
+        //==============================================================================================================
+
+        // ct_data = SE.Enc(data,k_data)
+        let randomness: symmetric::Randomness<_> = symmetric::Randomness {
+            r: self.ct_r.clone().unwrap(),
+        };
+        let ct_r =
+            symmetric::constraints::RandomnessVar::new_witness(ark_relations::ns!(cs, "r"), || {
+                Ok(randomness)
+            })
+            .unwrap();
+
+        let binding = self.ct_data.clone().unwrap();
+
+        let ct_data: symmetric::constraints::CiphertextVar<C::BaseField> =
             <SymmetricEncryptionSchemeGadget<C::BaseField> as SymmetricEncryptionGadget<
                 SymmetricEncryptionScheme<C::BaseField>,
                 C::BaseField,
-            >>::SymmetricKeyVar::new_witness(
-                ark_relations::ns!(cs, "k_data_2"),
+            >>::CiphertextVar::new_witness(
+                ark_relations::ns!(cs, "ct_data"),
                 || {
-                    Ok(symmetric::SymmetricKey {
-                        k: self.k_data.unwrap(),
+                    Ok(symmetric::Ciphertext {
+                        r: self.ct_r.unwrap(),
+                        c: binding,
                     })
-                },
+                }, // tk_addr_ena_old
             )
             .unwrap();
 
-        let randomness: symmetric::Randomness<_> = symmetric::Randomness {
-            r: self.ct_r.unwrap(),
-        };
-
-        let ct_r = symmetric::constraints::RandomnessVar::<C::BaseField>::new_witness(
-            ark_relations::ns!(cs, "r"),
-            || Ok(randomness),
+        let data = <SymmetricEncryptionSchemeGadget<C::BaseField> as SymmetricEncryptionGadget<
+            SymmetricEncryptionScheme<C::BaseField>,
+            C::BaseField,
+        >>::PlaintextVar::new_witness(
+            ark_relations::ns!(cs, "data"),
+            || {
+                Ok(symmetric::Plaintext {
+                    m: self.data.unwrap(),
+                })
+            }, // tk_addr_ena_old
         )
         .unwrap();
 
-        // let tmp = MiMCGadget::
+        let binding = self.k_data.clone().unwrap();
 
-        let ct_data: Vec<symmetric::constraints::CiphertextVar<C::BaseField>> = self
-            .ct_data
-            .clone()
-            .unwrap()
-            .iter()
-            .enumerate()
-            .map(|(i, c)| {
-                <SymmetricEncryptionSchemeGadget<C::BaseField> as SymmetricEncryptionGadget<
-                    SymmetricEncryptionScheme<C::BaseField>,
-                    C::BaseField,
-                >>::CiphertextVar::new_witness(
-                    ark_relations::ns!(cs, "ct_data{i}"),
-                    || {
-                        Ok(symmetric::Ciphertext {
-                            r: C::BaseField::from_bigint((i as u64).into()).unwrap(),
-                            c: *c,
-                        })
-                    },
-                )
-                .unwrap()
-            })
-            .collect();
-
-        let ct_data_2: Vec<FpVar<C::BaseField>> = self
-            .ct_data
-            .clone()
-            .unwrap()
-            .iter()
-            .map(|(i)| {
-                FpVar::new_witness(ark_relations::ns!(cs, "ct_data_2{i}"), || Ok(i)).unwrap()
-            })
-            .collect();
-
-        //==============================================================================================================
-
-        // h_k_data == Hash(pk_peer_own || k_data)
-        let check_h_ct = MiMCGadget::<C::BaseField>::evaluate(
-            &rc,
-            &[pk_peer_own.clone(), k_data_1.clone()].to_vec(),
+        let k_data = <SymmetricEncryptionSchemeGadget<C::BaseField> as SymmetricEncryptionGadget<
+            SymmetricEncryptionScheme<C::BaseField>,
+            C::BaseField,
+        >>::SymmetricKeyVar::new_witness(
+            ark_relations::ns!(cs, "k_data"),
+            || Ok(symmetric::SymmetricKey { k: binding }),
         )
         .unwrap();
-        // assert_eq!(self.h_ct.unwrap(), check_h_ct.value().unwrap());
-        h_k_data.enforce_equal(&check_h_ct).unwrap();
 
-        //==============================================================================================================
-
-        // h_ct = Hash(CT_data)
-        let check_h_ct =
-            MiMCGadget::<C::BaseField>::evaluate(&rc, ct_data_2.clone().as_ref()).unwrap();
-
-        h_ct.enforce_equal(&check_h_ct).unwrap();
-
-        //==============================================================================================================
-
-        //ct_data = SE.Enc(data,k_data)
-        let check_ct_data = SymmetricEncryptionSchemeGadget::<C::BaseField>::encrypt(
+        let result_ct_data = SymmetricEncryptionSchemeGadget::<C::BaseField>::encrypt(
             rc.clone(),
-            ct_r.clone(),
-            k_data_2.clone(),
-            symmetric::constraints::PlaintextVar { m: data.clone() },
+            ct_r,
+            k_data.clone(),
+            data.clone(),
         )
         .unwrap();
 
-        ct_data_2.enforce_equal(&[check_ct_data.c])
+        result_ct_data.enforce_equal(&ct_data)
+        //==============================================================================================================
     }
+}
+
+//==============================================================================================================
+//==============================================================================================================
+//==============================================================================================================
+use crate::gadget::hashes::CRHScheme;
+type C = ark_ed_on_bn254::EdwardsProjective;
+type GG = ark_ed_on_bn254::constraints::EdwardsVar;
+
+type F = ark_bn254::Fr;
+type H = mimc7::MiMC<F>;
+
+type SEEnc = symmetric::SymmetricEncryptionScheme<F>;
+
+#[allow(non_snake_case)]
+fn generate_test_input() -> Result<Registerdata<C, GG>, Error> {
+    let rng = &mut test_rng();
+    let rc: mimc7::Parameters<F> = mimc7::Parameters {
+        round_constants: mimc7::parameters::get_bn256_round_constants(),
+    };
+    //==============================================================================================================
+
+    let pk_peer_own = F::rand(rng);
+    let k_data = F::rand(rng);
+    let h_k_data =
+        H::evaluate(&rc.clone(), [pk_peer_own.clone(), k_data.clone()].to_vec()).unwrap();
+    //==============================================================================================================
+
+    let data = F::rand(rng);
+    let cin_r = F::rand(rng);
+    let sk = F::rand(rng);
+    let random = symmetric::Randomness { r: cin_r.clone() };
+    let key = symmetric::SymmetricKey { k: k_data };
+    let ct_data = SEEnc::encrypt(
+        rc.clone(),
+        random.clone(),
+        key.clone(),
+        symmetric::Plaintext { m: data },
+    )
+    .unwrap();
+
+    //==============================================================================================================
+
+    let h_ct = H::evaluate(&rc.clone(), [ct_data.clone().c].to_vec()).unwrap();
+
+    Ok(Registerdata {
+        rc: rc.clone().round_constants,
+        h_ct: Some(h_ct),
+        h_k_data: Some(h_k_data),
+        pk_peer_own: Some(pk_peer_own),
+        data: Some(data),
+        k_data: Some(k_data),
+        ct_r: Some(cin_r),
+        ct_data: Some(ct_data.c),
+        _curve_var: std::marker::PhantomData,
+    })
 }
 
 #[test]
 fn test_Data() {
-    println!("{:?}", 11);
+    let mut rng = ark_std::rand::rngs::StdRng::seed_from_u64(test_rng().next_u64());
+    println!("\nGenerate input!\n");
+
+    let test_input = generate_test_input().unwrap();
+
+    let (pk, vk) = {
+        let c = test_input.clone();
+
+        Groth16::<Bn254>::setup(c, &mut rng).unwrap()
+    };
+
+    // println!("\npk = {:?}\n", vk);
+
+    println!("\nPrepared verifying key!\n");
+    let pvk = Groth16::<Bn254>::process_vk(&vk).unwrap();
+
+    println!("\nGenerate proof!\n");
+
+    let c = test_input.clone();
+    let proof = Groth16::<Bn254>::prove(&pk, c, &mut rng).unwrap();
+
+    println!("{:?}", proof);
+
+    let image: Vec<_> = vec![
+        test_input.h_ct.clone().unwrap(),
+        test_input.h_k_data.clone().unwrap(),
+        test_input.pk_peer_own.clone().unwrap(),
+    ];
+
+    let tmp = Groth16::<Bn254>::verify_with_processed_vk(&pvk, &image, &proof).unwrap();
+    let tmp2 = Groth16::<Bn254>::verify(&vk, &image, &proof).unwrap();
+    println!("\nresult = {:?}", tmp2);
 }
